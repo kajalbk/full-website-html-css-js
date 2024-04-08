31a797f4a6d0c2db92daa9f8ab2269d2aaa46eb5
kajal sahani <166373141+kajalbk@users.noreply.github.com>
2024-04-08 15:32:39 +0530

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,13 @@
+function myfun2() {
+    document.getElementById('demo').innerHTML = 'gallery';
+}
+
+
+function myfun1() {
+    document.getElementById('demo').innerHTML = 'gallery images';
+}
+
+function myfun3() {
+    document.getElementById('img1').src = 'img/about.png';
+}
+
